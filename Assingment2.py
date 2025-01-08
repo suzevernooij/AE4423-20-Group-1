c99b7751838a7d91fdc1a22304b6f50ccb4fff06
juliamichels <j.h.michels@student.tudelft.nl>
2025-01-08 16:04:45 +0100

    Add files via upload

--- Assingment2.py
+++ Assingment2.py
@@ -34,25 +34,27 @@ demand_data = r"C:\TIL\Jaar 2\Airline Planning\Opdracht2\AE4423Ass2\Group1.xlsx"
 
 #Defining Parameters Fleet
 fleet       = pd.read_excel(fleet_data, skiprows=0, header=0, index_col=0)
-speed       = fleet.iloc[0].tolist()
-capacity    = fleet.iloc[1].tolist()
-TAT         = fleet.iloc[2].tolist()
-R           = fleet.iloc[3].tolist()
-runway_AC   = fleet.iloc[4].tolist()
-Cl          = fleet.iloc[5].tolist()
-Cf          = fleet.iloc[6].tolist()
-Ch          = fleet.iloc[7].tolist()
-cfuel       = fleet.iloc[8].tolist()
-fleet       = fleet.iloc[9].tolist()
+speed       = np.array(fleet.iloc[0])
+capacity    = np.array(fleet.iloc[1])
+TAT         = np.array(fleet.iloc[2])
+R           = np.array(fleet.iloc[3])
+runway_AC   = np.array(fleet.iloc[4])
+Cl          = np.array(fleet.iloc[5])
+Cf          = np.array(fleet.iloc[6])
+Ch          = np.array(fleet.iloc[7])
+cfuel       = np.array(fleet.iloc[8])
+fleet       = np.array(fleet.iloc[9])
 
 #Defining Parameters Airport
 airport     = pd.read_excel(airport_data, skiprows=0, header=0, index_col=0)
-IATA        = airport.iloc[0].tolist()
+IATA        = np.array(airport.iloc[0])
 latitude    = airport.iloc[1].tolist()
 longitude   = airport.iloc[2].tolist()
-runway_AP   = airport.iloc[3].tolist()
+runway_AP   = np.array(airport.iloc[3])
 
 Re = 6371 
+f = 1.42
+Yield = 0.26
 
 #Defining Sets
 n = len(IATA)
@@ -70,6 +72,20 @@ for i in range(n):
         a = math.sin(delta_lat / 2)**2 + math.cos(lat_rad[i]) * math.cos(lat_rad[j]) * math.sin(delta_lon / 2)**2
         sigma = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         d[i, j] = Re * sigma
+d = np.array(d)
 
+total_costs = [np.zeros((n,n)), np.zeros((n,n)), np.zeros((n,n))]
+for k in K:
+    for i in N:
+        for j in N:
+            if i == j:
+                CF = 0
+            else:
+                CF = Cf[k] 
+            CT = Ch[k] * d[i,j] / speed[k]
+            CFUEL = (cfuel[k] * f / 1.5) * d[i,j]
+            total = CF + CT + CFUEL
+            total_costs[k][i,j] = total
+            
 
 
